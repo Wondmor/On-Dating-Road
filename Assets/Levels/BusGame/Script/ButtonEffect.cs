c582725c87364b8db58ad02f039d33fc19d4e58a
Wondmor <Wza87656127@gmail.com>
2023-09-30 18:29:57 +0800

    BusGame完成

--- Assets/Levels/BusGame/Script/ButtonEffect.cs
+++ Assets/Levels/BusGame/Script/ButtonEffect.cs
@@ -1,15 +1,72 @@
 using System.Collections;
 using System.Collections.Generic;
+using Fungus;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
 
 public class ButtonEffect : MonoBehaviour
 {
-    
+    public RectTransform yesRectTransform;
+    public RectTransform noRectTransform;
 
-    public void OnPointerEnter(PointerEventData eventData)
+    public Camera mainCamera;
+
+    public void Update()
     {
-        Debug.Log("鼠标");
-        transform.localScale = new Vector3(1.5f, 1.5f, 1.5f);
+        Vector3 mouseScreenPosition = Input.mousePosition;
+
+        Vector3 mouseWorldPosition = mainCamera.ScreenToWorldPoint(mouseScreenPosition);
+
+        if(mouseWorldPosition.x > 1)
+        {
+            
+            if (yesRectTransform != null)
+            {
+                float newWidth = 150f;
+                float newHeight = 270f;
+                Vector2 sizeDelta = yesRectTransform.sizeDelta;
+                sizeDelta.x = newWidth;
+                sizeDelta.y = newHeight;
+                yesRectTransform.sizeDelta = sizeDelta;
+            }
+            if (noRectTransform != null)
+            {
+                float newWidth = 75f;
+                float newHeight = 175f;
+                Vector2 sizeDelta = noRectTransform.sizeDelta;
+                sizeDelta.x = newWidth;
+                sizeDelta.y = newHeight;
+                noRectTransform.sizeDelta = sizeDelta;
+            }
+        }
+
+        if(mouseWorldPosition.x < -1)
+        {
+            
+            if (yesRectTransform != null)
+            {
+                float newWidth = 75f;
+                float newHeight = 135f;
+                Vector2 sizeDelta = yesRectTransform.sizeDelta;
+                sizeDelta.x = newWidth;
+                sizeDelta.y = newHeight;
+                yesRectTransform.sizeDelta = sizeDelta;
+            }
+            if (noRectTransform != null)
+            {
+                float newWidth = 150f;
+                float newHeight = 350f;
+                Vector2 sizeDelta = noRectTransform.sizeDelta;
+                sizeDelta.x = newWidth;
+                sizeDelta.y = newHeight;
+                noRectTransform.sizeDelta = sizeDelta;
+            }
+        }
+        
     }
+
+
+
 }
