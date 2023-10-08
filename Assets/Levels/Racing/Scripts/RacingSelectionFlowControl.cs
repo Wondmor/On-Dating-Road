d8b70a4be03c0f04f0235599e0bbbad7bb0c0032
Eterne92 <36848989+eterne92@users.noreply.github.com>
2023-10-09 03:34:42 +0800

    Fix bugs

--- Assets/Levels/Racing/Scripts/RacingSelectionFlowControl.cs
+++ Assets/Levels/Racing/Scripts/RacingSelectionFlowControl.cs
@@ -18,6 +18,7 @@ public class RacingSelectionFlowControl : MonoBehaviour
         }
         else
         {
+            FungusManager.Instance.MusicManager.StopMusic();
             GameLogicManager.Instance.OnMiniGameRefused();
         }
     }
